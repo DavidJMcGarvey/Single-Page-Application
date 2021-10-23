ed24f46af3cca8e5631c042557c8da9530f35d04
David McGarvey <42125523+DavidJMcGarvey@users.noreply.github.com>
2021-10-23 09:19:21 -0700

    add public dir

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ require('dotenv').config(); // read .env files
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT = 3000;
+const port = process.env.PORT || 3000;
+
+// Set public folder as root
+app.use(express.static('public'))
+
+// Allow front-end access to node_modules dir
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
