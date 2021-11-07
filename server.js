2394adbb8b2921df565d2d3ea3c5854fb06ae664
David McGarvey <42125523+DavidJMcGarvey@users.noreply.github.com>
2021-11-07 09:07:54 -0800

    error handler

--- server.js
+++ server.js
@@ -11,7 +11,30 @@ app.use(express.static('public'))
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Express Error handler
+const errorHandler = (err, req, res) => {
+    if (err.response) {
+        // The request was made and the server responds with a status code
+        // that falls out of the range of 2xx
+        res.status(403).send({title: 'Server responded with an error', message: err.message});
+    } else if (err.request) {
+        // The request was made but no respnse was recieved
+        res.status(503).send({title: 'Unbale to communicate with server', message: err.message});
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        res.status(500).send({title: 'An unexpected error occured', message: err.message});
+    }
+};
 
+// Fetch Latest Currency Rates
+app.get('/api/rates', async (req, res) => {
+    try {
+        const data = await getRates();
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    } catch (error) {
+        errorHandler(error, req, res);
+    }
+});
 
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
